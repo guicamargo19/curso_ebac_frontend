1ab0f60c4a991edb27cf54f53e6c2879688ffc17
camargo <gui.ferreira.camargo@gmail.com>
2024-01-04 12:38:25 -0300

    AJAX Module

---
+++ main.js
@@ -0,0 +1,79 @@
+// AJAX COM JAVASCRIPT
+
+/* document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById('btn-buscar-cep').addEventListener('click', function() {
+        // AJAX
+        const xhttp = new XMLHttpRequest();
+        const cep = document.getElementById('cep').value;
+
+        const endpoint = `https://viacep.com.br/ws/${cep}/json`;
+
+        xhttp.open('GET', endpoint);
+        xhttp.send();
+    });
+}) */
+
+// AJAX COM JQUERY
+
+$(document).ready(function() {
+    $("#cep").mask('00000-000');
+
+    $('#btn-buscar-cep').click(function(){
+        const cep = $('#cep').val();
+        const endpoint = `https://viacep.com.br/ws/${cep}/json`;
+        const botao = $(this);
+
+        $(botao).find('i').addClass('d-none');
+        $(botao).find('span').removeClass('d-none');
+
+
+        /* $.ajax(endpoint).done(function(resposta){
+            const logradouro = resposta.logradouro;
+            const bairro = resposta.bairro;
+            const cidade = resposta.localidade;
+            const estado = resposta.uf;
+            const endereco = `${logradouro}, ${bairro}, ${cidade}, ${estado}`;
+
+            $('#endereco').val(endereco);
+
+            setTimeout(function() {
+                $(botao).find('i').removeClass('d-none');
+                $(botao).find('span').addClass('d-none');
+            }, 500);
+
+        }); */
+
+// AJAX COM fetch API
+
+        fetch(endpoint)
+        .then(function(resposta) {
+            return resposta.json();
+        })
+        .then(function(json) {
+            const logradouro = json.logradouro;
+            const bairro = json.bairro;
+            const cidade = json.localidade;
+            const estado = json.uf;
+            const endereco = `${logradouro}, ${bairro}, ${cidade}, ${estado}`;
+
+            $('#endereco').val(endereco);
+        })
+        .catch(function(erro) {
+            alert("Ocorreu um erro ao buscar o endereço, tente novamente mais tarde.");
+        })
+        .finally(function() {
+            setTimeout(function() {
+                $(botao).find('i').removeClass('d-none');
+                $(botao).find('span').addClass('d-none');
+            }, 500);
+        });
+    });
+
+    $('#formulario-pedido').submit(function(event) {
+        event.preventDefault();
+
+        if ($('#nome').val().lenght == 0) {
+            throw new Error("Digite o nome");
+        }
+    });
+})
